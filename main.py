935f24fe74fe2a385b139b390400fc592c35d728
Sangam kumar <87137164+Sangam-kumar432@users.noreply.github.com>
2021-08-25 13:25:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,79 @@
-
-import pygame
+ 
 from pygame import mixer
 from tkinter import *
 import os
 
-def playsong():
-    currentsong=playlist.get(ACTIVE)
-    print(currentsong)
-    mixer.music.load(currentsong)
-    songstatus.set("Playing")
+# functions for playing song
+
+def play_song():
+    current_song=playlist.get(ACTIVE)
+    print("now playing  :  " + current_song)
+    mixer.music.load(current_song)
+    song_status.set("Playing Music")
     mixer.music.play()
 
-def pausesong():
-    songstatus.set("Paused")
+# functions to pause the currently playing song
+
+def pause_song():
+    if (song_status.get() != "Music Paused"):
+        song_status.set("Music Paused")
+        print(song_status.get())
     mixer.music.pause()
 
-def stopsong():
-    songstatus.set("Stopped")
+def stop_song():
+    if (song_status.get() != "Music Stopped"):
+        song_status.set("Music stopped")
+        print(song_status.get())
     mixer.music.stop()
 
-def resumesong():
-    songstatus.set("Resuming")
+# functions for resuming the paused song    
+
+def resume_song():
+    if (song_status.get() != "Resuming Music"):
+        song_status.set("Resuming Music")
+        print(song_status.get())
     mixer.music.unpause()
 
+    
+# window for music playing
+
 root=Tk()
-root.title('My ( sangam-kumar432 )  Music player project')
+root.geometry('800x400')
+root.config(bg='SlateGray3')
+root.title('sangam Music player project')
 
 mixer.init()
-songstatus=StringVar()
-songstatus.set("choosing")
+song_status=StringVar()
+song_status.set("choosing")
 
-#playlist---------------
+# playlist to keep songs accessed from local storage
 
-playlist=Listbox(root,selectmode=SINGLE,bg="DodgerBlue2",fg="white",font=('arial',15),width=40)
-playlist.grid(columnspan=5)
+playlist=Listbox(root,selectmode=SINGLE,bg="white", fg="black",font=('arial',15),height=10,width=100)
+playlist.grid(columnspan=4)
 
-os.chdir(r'F:\music from my phone\SnapTube Audio\nice')
+os.chdir(r'F:\music from my phone\SnapTube Audio\SnapTube Audio from phone')
 songs=os.listdir()
 for s in songs:
     playlist.insert(END,s)
 
-playbtn=Button(root,text="play",command=playsong)
-playbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-playbtn.grid(row=1,column=0)
+    
+# Buttons for palying , pausing and resuming the song 
 
-pausebtn=Button(root,text="Pause",command=pausesong)
-pausebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-pausebtn.grid(row=1,column=1)
+play_button=Button(root, text="play", command=play_song)
+play_button.config(font=('arial', 20), bg="DodgerBlue2", fg="white", padx=7, pady=7)
+play_button.grid(row=1, column=0)
 
-stopbtn=Button(root,text="Stop",command=stopsong)
-stopbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-stopbtn.grid(row=1,column=2)
+pause_button=Button(root, text="Pause", command=pause_song)
+pause_button.config(font=('arial', 20), bg="DodgerBlue2", fg="white", padx=7, pady=7)
+pause_button.grid(row=1, column=1)
 
-Resumebtn=Button(root,text="Resume",command=resumesong)
-Resumebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-Resumebtn.grid(row=1,column=3)
+stop_button=Button(root, text="Stop", command=stop_song)
+stop_button.config(font=('arial', 20), bg="DodgerBlue2", fg="white", padx=7, pady=7)
+stop_button.grid(row=2, column=0)
+
+Resume_button=Button(root, text="Resume", command=resume_song)
+Resume_button.config(font=('arial', 20), bg="DodgerBlue2", fg="white", padx=7, pady=7)
+Resume_button.grid(row=2, column=1)
 
 mainloop()
+print("music interface Ended.")
